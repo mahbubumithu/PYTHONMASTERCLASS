27c3d65ae26dc9380e1afbe47ac258f6d3a00058
Md Mahbub Uddin <mahbubumithu@gmail.com>
2024-10-28 14:51:35 +0600

    add solution 1

---
+++ solution1.py
@@ -0,0 +1,5 @@
+#take user input
+user_input = input("Enter your Messages: ")
+#check user input..if expected word is available then user input will print otherwise output will be blank.
+if "Enjoy" in user_input:
+    print(user_input)
