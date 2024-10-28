7946615e3cc66ba93bc6b6d056a45a5de285948d
Md Mahbub Uddin <mahbubumithu@gmail.com>
2024-10-28 16:15:51 +0600

    add solution3

---
+++ solution3.py
@@ -0,0 +1,11 @@
+#take an integer input from user
+n = int(input("Enter an integer: "))
+
+if n % 3 == 0 and n % 5 == 0:
+    print("FizzBuzz")
+elif n % 5 == 0:
+    print("Buzz")
+elif n % 3 == 0:
+    print("Fizz")
+else:
+    print("Not a FizzBuzz Number")    
